878c36d84ec6143e0f23b1ea601ceedeaf460893
Micahel Thornton <michael.s.thornton@gmail.com>
2022-11-09 15:47:05 -0500

    Adding Michael Thornton Player.

---
+++ MacPig/MacPig/Players/MichaelThorntonPlayer.swift
@@ -0,0 +1,30 @@
+//
+//  MichaelThorntonPlayer.swift
+//  MacPig
+//
+//  Created by Michael Thornton on 11/9/22.
+//  Copyright © 2022 Michael Thornton. All rights reserved.
+//
+
+import Foundation
+
+
+class MichaelThorntonPlayer: PlayerProtocol {
+    
+    
+    func willPlayToTotalOf(_ goal: Int, withTurnScoreOf turnScore: Int, withPlayerTotal currentTotal: Int, againstOpponentWith opponentTotal: Int) -> Bool {
+        
+        return true
+    }
+    
+} // end class
+
+
+
+extension MichaelThorntonPlayer: CustomStringConvertible {
+    
+    var description: String {
+        return "Randomize It"
+    }
+    
+} // end extension
