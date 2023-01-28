b577d17a246b80d5bbbe28e8879f1d1e60b97edd
Owen Willer <108071643+hiiyasu@users.noreply.github.com>
2023-01-28 03:21:44 -0500

    Update OwenWillerPlayer.swift

--- MacPig/MacPig/Players/OwenWillerPlayer.swift
+++ MacPig/MacPig/Players/OwenWillerPlayer.swift
@@ -3,7 +3,7 @@ struct OwenWillerPlayer : PlayerProtocol {
         if turnScore + currentTotal < ((goal - currentTotal + turnScore) / 4) + opponentTotal {
           return true
       }
-        else if goal - turnScore + currentTotal == 20{
+        else if goal - turnScore + currentTotal <= 20{
           return true
       }
       return false
