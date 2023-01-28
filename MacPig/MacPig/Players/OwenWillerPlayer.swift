adb420839fa95b8abf9a47629391a08c5b888d5b
Owen Willer <108071643+hiiyasu@users.noreply.github.com>
2023-01-28 03:17:34 -0500

    Create OwenWillerPlayer.swift

---
+++ MacPig/MacPig/Players/OwenWillerPlayer.swift
@@ -0,0 +1,12 @@
+struct OwenWillerPlayer : PlayerProtocol {
+  func willPlayToTotalOf(_ goal: Int, withTurnScoreOf turnScore: Int, withPlayerTotal currentTotal: Int, againstOpponentWith opponentTotal: Int) -> Bool{
+        if turnScore + currentTotal < ((goal - currentTotal + turnScore) / 4) + opponentTotal {
+          return true
+      }
+        else if goal - turnScore + currentTotal == 20{
+          return true
+      }
+      return false
+  }
+}
+
